ff871c2965433c15602e2b6528cd08c27e5af7ad
nacey5 <1160124552@qq.com>
2022-12-30 16:20:39 +0800

    article -lop

--- models/models.go
+++ models/models.go
@@ -14,6 +14,7 @@ type Model struct {
 	ID         int `gorm:"primary_key" json:"id"`
 	CreatedOn  int `json:"created_on"`
 	ModifiedOn int `json:"modified_on"`
+	DeletedOn  int `json:"deleted_on"`
 }
 
 func init() {
--- models/tag.go
+++ models/tag.go
@@ -7,6 +7,7 @@ import (
 
 type Tag struct {
 	Model
+
 	Name       string `json:"name"`
 	CreatedBy  string `json:"created_by"`
 	ModifiedBy string `json:"modified_by"`
